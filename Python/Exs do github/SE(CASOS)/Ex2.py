d9b1fcbdc5c115d732885daf1d0848cd7dc33d9a
André <andreluiz.pedronila@gmail.com>
2024-05-28 16:44:07 -0300

    Update Ex2.py

--- Python/Exs do github/SE(CASOS)/Ex2.py
+++ Python/Exs do github/SE(CASOS)/Ex2.py
@@ -1,1 +1,13 @@
-    
+tc = input("Tipo de Carro | G para Gasolina e A para Álcool: ").strip()
+
+ct = int(input("Informe a capacidade do tanque em litros: "))
+
+if tc.upper() == "A":
+    print("O tipo do carro que foi escolhido é Álcool")
+else:
+    if tc.upper() == "G":
+        print("O tipo de carro que foi escolhido é Gasolina")
+
+preco = float(input("Informe o preço do combustível que condiz com o tipo do carro: "))
+valor = ct * preco
+print(f"O preço a ser pago para encher o tanque de combustível é de: R$ {valor:,.2f}")
